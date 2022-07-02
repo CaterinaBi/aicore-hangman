b47e5246ea64bd6163f51cf71b4dd3cd1f62df75
CaterinaBi <catherinebonan@gmail.com>
2022-07-02 21:06:05 +0100

    Final checks

--- hangman/hangman_solution.py
+++ hangman/hangman_solution.py
@@ -10,7 +10,6 @@ class Hangman:
     A Hangman Game that asks the user for a letter and checks if it is in the word.
     It starts with a default number of lives and a random word from the word_list.
 
-    
     Parameters:
     ----------
     word_list: list
